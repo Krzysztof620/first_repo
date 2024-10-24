b1fd9259946cf2b8de1080f4247d8e1d5a658bfd
govindnirav <87795211+govindnirav@users.noreply.github.com>
2024-10-24 16:22:00 +0100

    Added a special function just for you Krzys

---
+++ function.py
@@ -0,0 +1,4 @@
+def pull_rev(x):
+    # Enter name into function. It will print the following personalised text.
+    print ("Please approve my pull review " + x + ". I will be eternally grateful!")
+    
